639d81c77e442058277b929d3a8bff5c3411d717
black-knight <dani_ande@yahoo.dk>
2012-09-12 21:39:16 +0200

    Texture sizes power of two

--- Demoteket/Textures.m
+++ Demoteket/Textures.m
@@ -148,7 +148,7 @@ void textureSetBlend(Texture *texture, GLenum blendSrc, GLenum blendDst) {
 
     trollsAheadLogoTexture = [self loadTexture:@"trolls_ahead_logo.png"]; textureSetBlend(&trollsAheadLogoTexture, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    photoLoadingTexture = [self loadTexture:@"loading_photo.png"]; textureSetBlend(&photoLoadingTexture, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    photoLoadingTexture = [self loadTexture:@"loading_photo.png"]; textureSetBlend(&photoLoadingTexture, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); textureSetTexCoords(&photoLoadingTexture, 0.0f, 0.1679f, 1.0f, 1.0f - 0.1679f);
     photoBackgroundTexture = [self loadTexture:@"photo_background.png"];
 
     floorDistortionTexture = [self loadTexture:@"floor_distortion.png" repeat:true]; textureSetTexCoords(&floorDistortionTexture, 0.0f, 0.0f, 55.0f, 55.0f);
